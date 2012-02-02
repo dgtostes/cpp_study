5360a213f036f701bcf568d1ed5db07e33f4a3a0
diego <diego@humaita.(none)>
2012-02-01 23:42:22 -0200

    add get_quote_from_instrument_list

--- py_codes/finance/newStockLib.py
+++ py_codes/finance/newStockLib.py
@@ -7,27 +7,54 @@ from xml.dom import minidom
 from lxml import etree
 from BeautifulSoup import BeautifulStoneSoup
 
+def get_quote_from_instrument(instrument):
+    url = "http://www.bmfbovespa.com.br/cotacoes2000/formCotacoesMobile.asp?codsocemi=%s"
+    instrument_url = url % instrument
+    texto = urllib.urlopen(instrument_url)
+    soup = BeautifulStoneSoup(texto)
+    infos = soup.findAll('papel')
+    dic = {}
+    for info in infos:
+        dic['desc'] = info['descricao']
+        dic['instrument_code'] = info['codigo']
+        dic['is_bov'] = info['ibovespa']
+        dic['delay'] = info['delay']
+        dic['hour'] = info['hora']
+        dic['osc'] = info['oscilacao']
+        dic['last_price'] = info['valor_ultimo']
+        dic['vol'] = info['quant_neg']
+        dic['market'] = info['mercado']
 
-
-
-
-url = "http://www.bmfbovespa.com.br/cotacoes2000/formCotacoesMobile.asp?codsocemi=%s"
-instrument_url = url % "GOLL4"
-
-texto = urllib.urlopen(instrument_url)
-soup = BeautifulStoneSoup(texto)
-#print BeautifulStoneSoup(texto, selfClosingTags=['papel']).prettify()
-infos = soup.findAll('papel')
-dic = {}
-for info in infos:
-    dic['desc'] = info['descricao']
-    dic['instrument_code'] = info['codigo']
-    dic['is_bov'] = info['ibovespa']
-    dic['delay'] = info['delay']
-    dic['hour'] = info['hora']
-    dic['osc'] = info['oscilacao']
-    dic['last_price'] = info['valor_ultimo']
-    dic['vol'] = info['quant_neg']
-    dic['market'] = info['mercado']
-
-print dic
+    return dic
+    
+def get_quote_from_instrument_list(instrument_list):
+    list_2_return = []
+    param = '|'.join(instrument_list)
+    url2 = "http://www.bmfbovespa.com.br/Pregao-Online/ExecutaAcaoAjax.asp?CodigoPapel=%s" % param
+    texto = urllib.urlopen(url2)
+    soup = BeautifulStoneSoup(texto)
+    infos = soup.findAll('papel')
+    key_list = {"codigo":"code",
+                "nome": "instrument",
+                "ibovespa":"is_bovespa",
+                "data":"date",
+                "abertura":"open",
+                "minimo":"min",
+                "maximo":"max",
+                "medio":"avg",
+                "ultimo":"close",
+                "oscilacao":"osc",
+                "minino":"min2"}
+    for info in infos:
+        dic = {}
+        for key in key_list:
+            dic[key_list[key]] = info[key]
+        list_2_return.append(dic)
+        
+    return list_2_return          
+        
+    
+    
+if __name__ == "__main__":
+    #print get_quote_from_instrument("PETR4")
+    print get_quote_from_instrument_list(["PETR4","GOLL4"])
