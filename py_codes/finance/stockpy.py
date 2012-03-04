52789eb11ea408162d6f20ad914f3e32fd618469
diego <diego@humaita.(none)>
2012-03-04 11:49:14 -0300

    change self.self.dic_info initializator to get only the first elemente of the list_2_return of __get_quote_from_instrument_list(self)

--- py_codes/finance/stockpy.py
+++ py_codes/finance/stockpy.py
@@ -12,19 +12,19 @@ class Quotes(object):
     '''
     def __init__(self, instrument):
         self.instrument = instrument
-        self.dic_info = self.__get_quote_from_instrument_list()
+        self.dic_info = self.__get_quote_from_instrument_list()[0]
         self.history_dic = self.__history_dic()
-        self.expose = self.__expose_dic()        
+        self.expose = self.__expose_dic()
         self.history_line = self.__history_analysis()
-        
-   
+
+
     def __get_quote_from_instrument_list(self):
         instrument_list = [self.instrument]
         def process_float_string(float_string):
             if "," in float_string:
                 float_string = float_string.replace(",",".")
             return float(float_string)
-            
+
         def process_bovespa_xml_date_string(bovespa_xml_date_string):
             date_string = bovespa_xml_date_string[0:10]
             try:
@@ -35,10 +35,10 @@ class Quotes(object):
                 return datetime.date(year,month,day)
             except:
                 return datetime.date.today()
-            
-        
+
+
             return dic
-        
+
         list_2_return = []
         param = '|'.join(instrument_list)
         url2 = "http://www.bmfbovespa.com.br/Pregao-Online/ExecutaAcaoAjax.asp?CodigoPapel=%s" % param
@@ -62,16 +62,16 @@ class Quotes(object):
                 list_2_return.append(dic)
             except:
                 pass
-            
+
         return list_2_return
 
-    
+
     def __expose_dic(self):
         '''
-        this method return a string 
+        this method return a string
         whith quote informations abou the instrument
         '''
-        
+
         if self.__history_analysis():
             slope_quote = self.__history_analysis()['quote']['slope']
             a_tan_deg_quote = self.__history_analysis()['quote']['a_tan_deg']
@@ -82,15 +82,15 @@ class Quotes(object):
             a_tan_deg_quote = None
             slope_vol = None
             a_tan_deg_vol = None
-                    
+
         return "intrument: %s open: %s max: %s min: %s"\
               " last_price: %s osc: %s slope: %s deg: %s"\
-              " slope_vol: %s deg_vol: %s" %  (self.dic_info[0]['instrument'],
-                                                     self.dic_info[0]['open'],
-                                                     self.dic_info[0]['max'],
-                                                     self.dic_info[0]['min'],
-                                                     self.dic_info[0]['last_price'],
-                                                     self.dic_info[0]['osc'],
+              " slope_vol: %s deg_vol: %s" %  (self.dic_info['instrument'],
+                                                     self.dic_info['open'],
+                                                     self.dic_info['max'],
+                                                     self.dic_info['min'],
+                                                     self.dic_info['last_price'],
+                                                     self.dic_info['osc'],
                                                      slope_quote,
                                                      a_tan_deg_quote,
                                                      slope_vol,
@@ -100,14 +100,14 @@ class Quotes(object):
         today_day = today.day
         today_month = today.month - 1
         today_year = today.year
-        
+
         start = today - datetime.timedelta(days=7)
         start_day = start.day
         start_month = start.month -1
         start_year = start.year
         url_target = "http://ichart.finance.yahoo.com/"\
                      "table.csv?s=%s.SA&a=%s&b=%s&c=%s&d"\
-                     "=%s&e=%s&f=%s&g=d&ignore=.csv" % (self.dic_info[0]['instrument'],
+                     "=%s&e=%s&f=%s&g=d&ignore=.csv" % (self.dic_info['instrument'],
                                                         start_month,
                                                         start_day,
                                                         start_year,
@@ -120,25 +120,25 @@ class Quotes(object):
             html_list.pop(0)
             html_list.pop(len(html_list)-1)
             history_dic = {}
-            history_dic[self.dic_info[0]['instrument']] = {}
+            history_dic[self.dic_info['instrument']] = {}
             for i in html_list:
                 data_list = i.split(',')
                 date = process_date_string(data_list[0])
-                history_dic[self.dic_info[0]['instrument']][date] = {}        
+                history_dic[self.dic_info['instrument']][date] = {}
                 open_price = float(data_list[1])
                 max_price = float(data_list[2])
                 min_price = float(data_list[3])
                 close_price = float(data_list[4])
                 osc = close_price - open_price
                 vol = float(data_list[5].replace(",",""))
-                history_dic[self.dic_info[0]['instrument']][date] = {
+                history_dic[self.dic_info['instrument']][date] = {
                                                      'max_price':max_price,
                                                      'min_price':min_price,
                                                      'close_price':close_price,
                                                      'osc':osc,
                                                      'vol':vol
                                                      }
-                                                         
+
             return history_dic
         except:
             return None
@@ -155,20 +155,20 @@ class Quotes(object):
                                                  self.dic_info['min'],
                                                  self.dic_info['last_price'],
                                                  self.dic_info['osc'])
-        
+
         write_line(self.history_file,line)
-        
+
 
     def __history_analysis(self):
         '''
         this method process the __history_dic
-        generate a coordinate of the points of the 
+        generate a coordinate of the points of the
         history quote curve and using the best_line_apx
-        function, return informations about the best 
+        function, return informations about the best
         Straight-Line Equation that that approximates the points
         of the quote curve
         '''
-        instrument = self.dic_info[0]['instrument']
+        instrument = self.dic_info['instrument']
         dic = {}
         if self.history_dic:
             history_dic = self.history_dic
@@ -181,7 +181,7 @@ class Quotes(object):
                 coord_list_vol.append((date_list.index(i),history_dic[instrument][i]['vol']))
             dic['quote'] = best_line_apx(coord_list_quote)
             dic['vol'] = best_line_apx(coord_list_vol)
-            return dic    
+            return dic
 
         else:
             return None
@@ -196,18 +196,18 @@ class Quotes(object):
                 dic_osc[quote_info['osc']].append(quote_info)
             except:
                 dic_osc[quote_info['osc']] = [quote_info]
-    
+
         order_osc_list = sorted([i for i in dic_osc])
-            
+
         for osc in order_osc_list:
             for dic in dic_osc[osc]:
                 self._expose_dic(dic)
-        
+
 def write_line(file2save, line):
     f = open(file2save, 'a')
     f.write("%s\n" % line)
     f.close()
-                                                
+
 
 def process_date_string(date_string):
     '''
@@ -218,14 +218,14 @@ def process_date_string(date_string):
     year = int(date_list[0])
     month = int(date_list[1])
     day = int(date_list[2])
-    return datetime.date(year, month,day) 
+    return datetime.date(year, month,day)
 
 def best_line_apx(points_coor_list):
     '''
     recieves a list of tuples
     that represent points of a curve
-    return a tuple the terms of the 
-    line that best aprox of the curve 
+    return a tuple the terms of the
+    line that best aprox of the curve
     points. use the newton min sqt method
     '''
 
@@ -249,7 +249,7 @@ def best_line_apx(points_coor_list):
         a=(-sum_x*sum_xy+sum_xx*sum_y)/(n*sum_xx-sum_x*sum_x)
     except:
         a = None
-    try:    
+    try:
         b=(-sum_x*sum_y+n*sum_xy)/(n*sum_xx-sum_x*sum_x)
     except:
         b = None
@@ -262,11 +262,11 @@ def best_line_apx(points_coor_list):
     try:
         arc_tan_deg = (180.0/3.14)*arc_tan_rad
     except:
-        arc_tan_deg = None     
- 
-    return {'slope':b, 
-            'linear_c':a, 
-            'a_tan_rad':arc_tan_rad, 
+        arc_tan_deg = None
+
+    return {'slope':b,
+            'linear_c':a,
+            'a_tan_rad':arc_tan_rad,
             'a_tan_deg':arc_tan_deg}
 
 
